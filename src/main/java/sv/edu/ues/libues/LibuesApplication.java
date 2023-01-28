e1b820f18cde454cdffd846fc24d3e760db5be75
Vladimir Stanley Alvarez <stanleyvladimir2005@hotmail.com>
2023-01-27 23:40:47 -0600

    Initial commit

---
+++ src/main/java/sv/edu/ues/libues/LibuesApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.ues.libues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibuesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibuesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sv/edu/ues/libues/LibuesApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.ues.libues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibuesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
